5b3073c2214d829a175ccb8939dad9c1fd0bf0ef
Michael Medvedovski <52825236+mimed95@users.noreply.github.com>
2022-04-22 02:39:31 +0200

    annotated optional date arguments in line chart, added candle chart template

--- src/tickers.py
+++ src/tickers.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 from urllib.request import urlopen
 
 import pandas as pd
@@ -20,12 +21,17 @@ class ticker:
 
 def display_chart(
     name: str,
-    data_history: pd.Series,
+    data_history: pd.DataFrame,
     image_url: str,
     ticker: str,
-    start_date: str,
-    end_date: str,
+    start_date: Optional[str] = None,
+    end_date: Optional[str] = None,
 ):
+    if start_date is None:
+        start_date = data_history.index[0]
+    if end_date is None:
+        end_date = data_history.index[-1]
+    selected_data = data_history.Close.loc[start_date:end_date]
     st.write(f"{name.upper()} ($)")
     image = Image.open(urlopen(image_url))
     #    Display image
@@ -33,4 +39,9 @@ def display_chart(
     #    Display dataframe
     st.table(ticker)
     #    Display a chart
-    st.bar_chart(data_history.Close)
+    st.bar_chart(selected_data)
+
+
+def candlestick_chart(data, symbol):
+    # see https://neuralmarkettrends.com/python-tutorials/
+    pass
